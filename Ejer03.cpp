62fcd88cfd8536bf95bd35953abe13ee0b8f7ffb
00385219 <00385219@uca.edu.sv>
2021-01-16 11:50:23 -0600

    Adding new file

--- Ejer03.cpp
+++ Ejer03.cpp
@@ -23,8 +23,11 @@ int main(void)
 
     ofstream archivo("Suma_Promedio_N_Numeros.txt", ios::app);
     
+    cout << "\nAlmacenando la suma de los números que es: "<< suma << endl;
     archivo << "\nLa suma de los números es: "<< suma << endl;
+    cout << "\nAlmacenando el promedio de los números que es: "<< prom << endl;
     archivo << "El promedio de los números es: "<< prom << endl ;
     
+    cout << endl << "Datos almacenados en el archivo Suma_Promedio_N_Numeros.txt" << endl << endl ;
     getchar();
 }
